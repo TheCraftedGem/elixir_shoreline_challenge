eb7184f52e951a11e456ecef6e264a488c44fe15
Emilio <thecraftedgem@tuta.io>
2019-05-02 13:35:48 -0600

    initial commit

---
+++ lib/elixir_shoreline_challenge.ex
@@ -0,0 +1,18 @@
+defmodule ElixirShorelineChallenge do
+  @moduledoc """
+  Documentation for ElixirShorelineChallenge.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirShorelineChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_shoreline_challenge_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirShorelineChallengeTest do
+  use ExUnit.Case
+  doctest ElixirShorelineChallenge
+
+  test "greets the world" do
+    assert ElixirShorelineChallenge.hello() == :world
+  end
+end
